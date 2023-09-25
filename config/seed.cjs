51c63348f5214508e26db4e7d62a6b96ed2f4a54
SamteaSilver <tanakatoyoki19@gmail.com>
2023-09-25 11:30:33 -0500

    Mern Cafe completed

---
+++ config/seed.cjs
@@ -0,0 +1,108 @@
+require('dotenv').config();
+require('./database.cjs');
+
+const Category = require('../models/category.cjs');
+const Item = require('../models/item.cjs');
+
+(async function () {
+    await Category.deleteMany({});
+    const categories = await Category.create([
+        { name: 'Sandwiches', sortOrder: 10 },
+        { name: 'Seafood', sortOrder: 20 },
+        { name: 'Mexican', sortOrder: 30 },
+        { name: 'Italian', sortOrder: 40 },
+        { name: 'Sides', sortOrder: 50 },
+        { name: 'Desserts', sortOrder: 60 },
+        { name: 'Drinks', sortOrder: 70 },
+    ]);
+
+    await Item.deleteMany({});
+    const items = await Item.create([
+        {
+            name: 'Hamburger',
+            emoji: '🍔',
+            category: categories[0]._id,
+            price: 5.95,
+        },
+        {
+            name: 'Turkey Sandwich',
+            emoji: '🥪',
+            category: categories[0]._id,
+            price: 6.95,
+        },
+        { name: 'Hot Dog', emoji: '🌭', category: categories[0]._id, price: 3.95 },
+        {
+            name: 'Crab Plate',
+            emoji: '🦀',
+            category: categories[1]._id,
+            price: 14.95,
+        },
+        {
+            name: 'Fried Shrimp',
+            emoji: '🍤',
+            category: categories[1]._id,
+            price: 13.95,
+        },
+        {
+            name: 'Whole Lobster',
+            emoji: '🦞',
+            category: categories[1]._id,
+            price: 25.95,
+        },
+        { name: 'Taco', emoji: '🌮', category: categories[2]._id, price: 1.95 },
+        { name: 'Burrito', emoji: '🌯', category: categories[2]._id, price: 4.95 },
+        {
+            name: 'Pizza Slice',
+            emoji: '🍕',
+            category: categories[3]._id,
+            price: 3.95,
+        },
+        {
+            name: 'Spaghetti',
+            emoji: '🍝',
+            category: categories[3]._id,
+            price: 7.95,
+        },
+        {
+            name: 'Garlic Bread',
+            emoji: '🍞',
+            category: categories[3]._id,
+            price: 1.95,
+        },
+        {
+            name: 'French Fries',
+            emoji: '🍟',
+            category: categories[4]._id,
+            price: 2.95,
+        },
+        {
+            name: 'Green Salad',
+            emoji: '🥗',
+            category: categories[4]._id,
+            price: 3.95,
+        },
+        {
+            name: 'Ice Cream',
+            emoji: '🍨',
+            category: categories[5]._id,
+            price: 1.95,
+        },
+        { name: 'Cup Cake', emoji: '🧁', category: categories[5]._id, price: 0.95 },
+        { name: 'Custard', emoji: '🍮', category: categories[5]._id, price: 2.95 },
+        {
+            name: 'Strawberry Shortcake',
+            emoji: '🍰',
+            category: categories[5]._id,
+            price: 3.95,
+        },
+        { name: 'Milk', emoji: '🥛', category: categories[6]._id, price: 0.95 },
+        { name: 'Coffee', emoji: '☕', category: categories[6]._id, price: 0.95 },
+        { name: 'Mai Tai', emoji: '🍹', category: categories[6]._id, price: 8.95 },
+        { name: 'Beer', emoji: '🍺', category: categories[6]._id, price: 3.95 },
+        { name: 'Wine', emoji: '🍷', category: categories[6]._id, price: 7.95 },
+    ]);
+
+    console.log(items);
+
+    process.exit();
+})();
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import NewOrderPage from "../NewOrderPage/NewOrderPage.jsx"
-import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage.jsx"
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage.jsx'
 import AuthPage from "../AuthPage/AuthPage.jsx"
-import { Routes, Route } from 'react-router-dom'
-import NavBar from '../../components/NavBar/NavBar'
 import { getUser } from '../../utilities/users-service'
+
+
 function App() {
   const [user, setUser] = useState(getUser())
 
+
+
   return (
-    <main className='App'>
+    <main className="App">
       {
         user ?
           <>
-            <NavBar user={user} setUser={setUser} />
             <Routes>
-              <Route path="/orders" element={<OrderHistoryPage />} />
-              <Route path="/orders/new" element={<NewOrderPage />} />
-
+              <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
+              <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
+              <Route path="*" element={<Navigate to="/orders/new" />} />
             </Routes>
           </>
           :
